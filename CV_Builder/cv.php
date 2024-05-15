77a6d375b86706c07a88fd62ea3261ad7e34b024
Krishna-Aggarwal-11 <krishnaaggarwal11may@gmail.com>
2024-05-16 00:21:32 +0530

    finished

--- CV_Builder/cv.php
+++ CV_Builder/cv.php
@@ -9,9 +9,18 @@ if(isset($_POST['submit'])) {
     $address = $_POST['address'];
     $phone = $_POST['phone'];
     $email = $_POST['email'];
-    $dob = $_POST['dob'];
+    $dob = $_POST['date'];
+    $pic = $_FILES['pic']['name'];
+    $dir = 'images/' . basename($pic);
+    move_uploaded_file($_FILES['pic']['tmp_name'], $dir);
     
 
+
+    $description = $_POST['description'];
+
+    $experience = $_POST['experience'];
+
+    $education = $_POST['education'];
 }
 
 
@@ -28,36 +37,26 @@ $html = "
 </head>
 
 <body contenteditable>
-    <h1 class='naglowek'>Curriculum Vitae</h1>
+    <h1 class=''>Curriculum Vitae</h1>
     <h2>$name</h2>
     <p><button class='przycisk'>Remove contact details</button></p>
-    <h3 class='naglowek3'>Contact details</h3>
+    <h3 class=''>Contact details</h3>
     <p class='condet'>Adress: <strong>$address</strong><br>Phone: <strong>$phone</strong><br>E-mail:
         <strong>$email</strong><br>
         Date of birth: <strong>$dob</strong></p>
-    <img width='200' height='200' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSppkoKsaYMuIoNLDH7O8ePOacLPG1mKXtEng&usqp=CAU' alt='Daniel Żaczek'>
-    <p><i>Open to innovative solutions, technological novelties, incurable perfectionist, ready to take on new
-            challenges, <br>always willing to collaborate. Looking for an international employer where I can use my
-            language skills <br>and grow in a working environment with short deadlines that requires problem solving
-            skills.</i></p>
+    <img width='200' height='200' src='images/$pic' alt='$pic'>
+    <hr>
+    <h3 class=''>About me</h3>
+    <p>$description</p>
 
-    <h3 class='naglowek3'>Experience</h3>
-    <h4>Team Leader, Perfetti van Melle BV, <br>Breda (The Netherlands)</h4>
-    <p><span class='daty'> J U N E 2 0 2 2 — N O W A D A Y S</span><br><br>● planning team activities <b>(25
-            persons)</b><br>
-        ● resolving disputes between employees<br>● supervising the performance of team members' tasks<br>● motivating
-        and supporting employees<br>● representing the team in front of other departments in the company's structures
+    <hr>
+    <h3 class=''>Experience</h3>
+    <p>$experience
     </p>
-    <h4>All-round warehouse specialist, Ellis Enterprises B.V. / Valvoline,<br>Moerdijk, Dordrecht (The Netherlands)
-    </h4>
-    <p><span class='daty'>J A N U A R Y 2 0 1 9 — M A Y 2 0 2 2</span><br><br><em>● full warehouse service</em><br>●
-        preparation and shipment of
-        personalized orders to the Benelux countries<br>● reach truck and forklift maintenance<br>● loading and
-        unloading of goods<br>● exploiting Crude Oil Distillation Products</p>
 
-    <h3 class='naglowek3'>Education</h3>
-    <p>AGH University of Science and Technology, Cracow (Poland)<br>Materials and Technologies for Energy and
-        Aviation<br><br><span class='daty'>S E P T E M B E R 2 0 0 7 — J U N E 2 0 1 0</span></p>
+    <hr>
+    <h3 class=''>Education</h3>
+    <p>$education</p>
 
 
 </body>
@@ -71,11 +70,4 @@ $mpdf->WriteHTML($stylesheet, \Mpdf\HTMLParserMode::HEADER_CSS);
 $mpdf->WriteHTML($html, \Mpdf\HTMLParserMode::HTML_BODY);
 
 
-
-
-
-
-
-
-$mpdf->WriteHTML('<h1>Hello world!</h1>');
-$mpdf->Output();
+$mpdf->Output("cv.pdf", "d");
